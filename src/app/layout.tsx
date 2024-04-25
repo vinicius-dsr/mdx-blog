c785e1a60f2ffd3f5d16825ef8a677f714b5252e
unknown <viniciusreis3105@gmail.com>
2024-04-25 16:15:26 -0300

    add shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-br">
+      <body className={cn("npx create-next-app@latest", inter.variable)}>
+        {children}
+      </body>
     </html>
   );
 }
