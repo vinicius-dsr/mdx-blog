aa0507a80c0b4ee2721cb822ab0ad73e97ceb645
unknown <viniciusreis3105@gmail.com>
2024-04-26 14:37:59 -0300

    add mode toggle for change themes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,45 +5,35 @@ import { cn } from "@/lib/utils";
 import { buttonVariants } from "./ui/button";
 import MainNav from "./MainNav";
 import MobileNav from "./MobileNav";
+import ModeToggle from "./ModeToggle";
 
 export default function Header() {
   return (
     <header className="z-10 sticky top-0 w-full border-b border-border bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 justify-between max-w-screen-xl items-center">
-        <Link href="/" className="flex items-center gap-1">
-          <Icons.logo className="h-10 w-10" />
-          <span className="font-bold">Blog</span>
-        </Link>
+      <div className="container flex h-20 max-w-screen-xl items-center">
+        <div className="flex items-center gap-8">
+          <Link href="/" className="flex items-center gap-1">
+            <Icons.logo className="h-16 w-16" />
+            <span className="font-bold text-2xl">Blog</span>
+          </Link>
+          <MainNav />
+        </div>
 
-        <MainNav />
+        
 
-        <div className="flex items-center gap-1">
+        <div className="flex flex-1 items-center gap-1 justify-end">
           <Link href={siteConfig.links.github} target="_blank" rel="noreferrer">
             <div
               className={cn(
                 buttonVariants({ variant: "ghost" }),
-                "w-10 px-0 hidden sm:inline-flex"
+                "w-10 px-0 rounded-full hidden sm:inline-flex"
               )}
             >
               <Icons.github className="h-6 w-6" />
               <span className="sr-only">GitHub</span>
             </div>
           </Link>
-          <Link
-            href={siteConfig.links.instagram}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <div
-              className={cn(
-                buttonVariants({ variant: "ghost" }),
-                "w-10 px-0 hidden sm:inline-flex"
-              )}
-            >
-              <Icons.instagram className="h-6 w-6" />
-              <span className="sr-only">Instagram</span>
-            </div>
-          </Link>
+          <ModeToggle />
           <MobileNav />
         </div>
       </div>
--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,8 +1,9 @@
-"use client"
+"use client";
 
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import ModeToggle from "./ModeToggle";
 
 export default function MainNav() {
   const pathname = usePathname();
@@ -14,21 +15,27 @@ export default function MainNav() {
           "text-sm font-medium transition-colors hover:text-primary hidden sm:inline-block",
           pathname === "/blog" ? "text-foreground" : "text-foreground/60"
         )}
-      >Blog</Link>
+      >
+        Blog
+      </Link>
       <Link
         href="#latest"
         className={cn(
           "text-sm font-medium transition-colors hover:text-primary hidden sm:inline-block",
           pathname === "#latest" ? "text-foreground" : "text-foreground/60"
         )}
-      >Recentes</Link>
+      >
+        Recentes
+      </Link>
       <Link
         href="/about"
         className={cn(
           "text-sm font-medium transition-colors hover:text-primary hidden sm:inline-block",
           pathname === "/about" ? "text-foreground" : "text-foreground/60"
         )}
-      >Sobre</Link>
+      >
+        Sobre
+      </Link>
     </nav>
   );
 }
